ba31834fc6a61e2f5ab8c1b94bed33e24e6fd14e
Sky <82303914+skysheng7@users.noreply.github.com>
2024-11-02 21:37:49 -0700

    dynamic path added

--- src/AI_representation_bias_in_farming/utils.py
+++ src/AI_representation_bias_in_farming/utils.py
@@ -1,7 +1,9 @@
-import pandas as pd
-from openai import OpenAI
 import base64
 import os
+from pathlib import Path
+
+import pandas as pd
+from openai import OpenAI
 
 def get_prompt(country, farm_type, generation_type):
     """
@@ -10,7 +12,7 @@ def get_prompt(country, farm_type, generation_type):
     Parameters:
         country (str): which country should the image content be based on. options: pd.NA, Canada, the United States, Germany
         farm_type (str): which type of livestock farm shoulld the image depict. options: dairy, pig
-        generation_type (str): what type of text prompt is this. options: default, default_no_revise, default_country, reality_country
+        generation_type (str): what type of text prompt is this.
 
     Returns:
         cur_prompt (str): the generated tect prompt
@@ -20,21 +22,21 @@ def get_prompt(country, farm_type, generation_type):
     # read in the type and styles of generation in generation_type
     word_list = generation_type.split("_")
     type1 = word_list[0]
-    if (len(word_list) > 1):
+    if len(word_list) > 1:
         revise_note = word_list[-2] + "_" + word_list[-1]
     else:
         revise_note = ""
 
     # specify the first part of the prompt based on the generation_type
-    if (type1 == "reality"):
-        cur_prompt = "Please create an image that accurately represents the reality of what most " + farm_type + " farms look like"
-    elif (type1 == "90percent"):
-        cur_prompt = "Please create an image that accurately represents what 90%% of " + farm_type + " farms look like"
-    elif (type1 == "typical"):
+    if type1 == "reality":
+        cur_prompt = (
+            "Please create an image that accurately represents the reality of what most "
+            + farm_type
+            + " farms look like"
+        )
+    elif type1 == "typical":
         cur_prompt = "A typical " + farm_type + " farm"
-    elif (type1 == "default"):
-        cur_prompt = "Please create an image of a " + farm_type + " farm"
-    elif (type1 == "basic"):
+    elif type1 == "basic":
         cur_prompt = "A " + farm_type + " farm"
 
     # add the specific country to the prompt if there is "country" in the generation type
@@ -42,13 +44,15 @@ def get_prompt(country, farm_type, generation_type):
         cur_prompt = cur_prompt + " in " + country + "."
     else:
         cur_prompt = cur_prompt + "."
-        
-    
+
     # openAI automatically revise user prompt. try to stop that
-    if (revise_note == "no_revise"):
-        cur_prompt = cur_prompt + "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:"
+    if revise_note == "no_revise":
+        cur_prompt = (
+            cur_prompt
+            + "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:"
+        )
         # openAI API note: "While it is not currently possible to disable this feature, you can use prompting to get outputs closer to your requested image by adding the following to your prompt:I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:"
-    
+
     return cur_prompt
 
 
@@ -60,27 +64,25 @@ def prompt_for_img(client, country, farm_type, generation_type):
         client: openAI API client for generating images
         country (str): which country should the image content be based on. options: pd.NA, Canada, the United States, Germany
         farm_type (str): which type of livestock farm shoulld the image depict. options: dairy, pig
-        generation_type (str): what type of text prompt is this. options: default, default_no_revise, default_country, reality_country
+        generation_type (str): what type of text prompt is this.
 
     Returns:
         response: the response from API call
 
     """
     cur_prompt = get_prompt(country, farm_type, generation_type)
-    
+
     response = client.images.generate(
-    model="dall-e-3",
-    prompt= cur_prompt,
-    size="1024x1024",
-    quality="standard",
-    response_format = "b64_json",
-    n=1,
+        model="dall-e-3",
+        prompt=cur_prompt,
+        size="1024x1024",
+        quality="standard",
+        response_format="b64_json",
+        n=1,
     )
 
-    return {
-        "response": response,
-        "prompt": cur_prompt
-    }
+    return {"response": response, "prompt": cur_prompt}
+
 
 def gen_image_train(client, country, farm_type, generation_type, start_index, n):
     """
@@ -90,7 +92,7 @@ def gen_image_train(client, country, farm_type, generation_type, start_index, n)
         client: openAI API client for generating images
         country (str): which country should the image content be based on. options: pd.NA, Canada, the United States, Germany
         farm_type (str): which type of livestock farm shoulld the image depict. options: dairy, pig
-        generation_type (str): what type of text prompt is this. options: default, default_no_revise, default_country, reality_country
+        generation_type (str): what type of text prompt is this.
         start_index (int): the start index (ID) of the generated image in a roll
         n (int): how many images you want to generate starting from the start index
 
@@ -98,19 +100,23 @@ def gen_image_train(client, country, farm_type, generation_type, start_index, n)
         None
 
     """
-    # generate n images 
-    for img_count in range(start_index, (start_index+n)):
+    # generate n images
+    for img_count in range(start_index, (start_index + n)):
         # prompt API for a image response
-        result = prompt_for_img(client, country, farm_type, generation_type) 
-        response = result["response"] 
-        cur_prompt = result["prompt"] # generate prompt
+        result = prompt_for_img(client, country, farm_type, generation_type)
+        response = result["response"]
+        cur_prompt = result["prompt"]  # generate prompt
 
-        image_data = response.data[0].b64_json 
-        revised_input = response.data[0].revised_prompt # get what GPT-4o automatically rephrased the prompt into
+        image_data = response.data[0].b64_json
+        revised_input = response.data[
+            0
+        ].revised_prompt  # get what GPT-4o automatically rephrased the prompt into
         image_bytes = base64.b64decode(image_data)  # get the image data
 
         save_imag(generation_type, img_count, farm_type, image_bytes, country)
-        save_megadata(img_count, country, farm_type, generation_type, cur_prompt, revised_input)
+        save_megadata(
+            img_count, country, farm_type, generation_type, cur_prompt, revised_input
+        )
 
 
 def save_imag(generation_type, img_count, farm_type, image_bytes, country):
@@ -118,33 +124,47 @@ def save_imag(generation_type, img_count, farm_type, image_bytes, country):
     Save the generated images into the local folder
 
     Parameters:
-        generation_type (str): what type of text prompt is this. options: default, default_no_revise, default_country, reality_country
+        generation_type (str): what type of text prompt is this.
         img_count (int): the current index (ID) of the generated image
         farm_type (str): which type of livestock farm shoulld the image depict. options: dairy, pig
         country (str): which country should the image content be based on. options: pd.NA, Canada, the United States, Germany
-        
+
     Returns:
         None
 
     """
 
     # define output dir
-    img_dir = "./results/images/" + generation_type + "/"   
-    if not os.path.exists(img_dir): 
-        os.makedirs(img_dir) # create the directory if the directory does not exist
+    img_dir = Path() / "results" / "images" / generation_type
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)  # create the directory if the directory does not exist
 
     # define filename
-    if (pd.isna(country)):
-        file_name = farm_type + "_farm_" + generation_type + "_" + str(img_count) + ".png"
+    if pd.isna(country):
+        file_name = (
+            farm_type + "_farm_" + generation_type + "_" + str(img_count) + ".png"
+        )
     else:
-        file_name = farm_type + "_farm_" + generation_type + "_" + country + "_" + str(img_count) + ".png"
+        file_name = (
+            farm_type
+            + "_farm_"
+            + generation_type
+            + "_"
+            + country
+            + "_"
+            + str(img_count)
+            + ".png"
+        )
 
     # Save the image locally
-    image_name = img_dir + file_name
-    with open(image_name, 'wb') as image_file:
+    image_name = img_dir / file_name
+    with open(image_name, "wb") as image_file:
         image_file.write(image_bytes)
 
-def save_megadata(img_count, country, farm_type, generation_type, cur_prompt, revised_input):
+
+def save_megadata(
+    img_count, country, farm_type, generation_type, cur_prompt, revised_input
+):
     """
     Save the generated images into the local folder
 
@@ -162,36 +182,49 @@ def save_megadata(img_count, country, farm_type, generation_type, cur_prompt, re
     """
 
     # define output dir
-    data_output_dir = "./results/megadata/"
+    data_output_dir = Path() / "results" / "megadata"
 
     # define filename
-    if (pd.isna(country)):
-        file_name = farm_type + "_farm_" + generation_type + "_" + str(img_count) + ".png"
+    if pd.isna(country):
+        file_name = (
+            farm_type + "_farm_" + generation_type + "_" + str(img_count) + ".png"
+        )
     else:
-        file_name = farm_type + "_farm_" + generation_type + "_" + country + "_" + str(img_count) + ".png"
+        file_name = (
+            farm_type
+            + "_farm_"
+            + generation_type
+            + "_"
+            + country
+            + "_"
+            + str(img_count)
+            + ".png"
+        )
 
     # Save the megadata
-    result_df = pd.DataFrame({
-        "file": [file_name],
-        "generation_type": [generation_type],
-        "country": [country],
-        "farm_type": [farm_type],
-        "prompt": [cur_prompt],
-        "revised_prompt": [revised_input],
-        "model": ["dall-e-3"],
-        "size": ["1024x1024"],
-        "quality": ["standard"],
-        "response_format": ["b64_json"]
-    })
-    megadata_file = data_output_dir + "image_megadata.csv"
+    result_df = pd.DataFrame(
+        {
+            "file": [file_name],
+            "generation_type": [generation_type],
+            "country": [country],
+            "farm_type": [farm_type],
+            "prompt": [cur_prompt],
+            "revised_prompt": [revised_input],
+            "model": ["dall-e-3"],
+            "size": ["1024x1024"],
+            "quality": ["standard"],
+            "response_format": ["b64_json"],
+        }
+    )
+    megadata_file = data_output_dir / "image_megadata.csv"
 
     if os.path.exists(megadata_file):
         # Load the existing dataframe
         existing_df = pd.read_csv(megadata_file)
         # Append new rows
-        combined_df = pd.concat([existing_df, result_df], ignore_index = True)
+        combined_df = pd.concat([existing_df, result_df], ignore_index=True)
     else:
         # If file does not exist, the result_df is the combined dataframe
         combined_df = result_df
-    
-    combined_df.to_csv(megadata_file, index = False)
+
+    combined_df.to_csv(megadata_file, index=False)
