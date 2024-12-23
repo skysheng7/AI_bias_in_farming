ac1fda4d80d2cc6b414e1894d778535d5a79f02a
Sky <82303914+skysheng7@users.noreply.github.com>
2024-12-22 22:35:50 -0800

    small update

--- scripts/01-text_to_image.py
+++ scripts/01-text_to_image.py
@@ -91,7 +91,7 @@ def main(start_index, total_image_num, model):
         "WARNING: This may take 10 minutes to a couple hours to run depending on how many images you wish to generate"
     )
     print(
-        "This will create multiple new images based on text prompts using DALL-E 3 or Stable Diffusion 3.5-large (n images per unique prompts, we have 48 unique prompts in total, with n=total_image_num). Generated images will be in results/dall-e-3-images, image metadata will be stored in results/megadata/image_megadata.csv."
+        "This will create multiple new images based on text prompts using DALL-E 3 or Stable Diffusion 3.5-large (n images per unique prompts, we have 48 unique prompts in total, with n=total_image_num). Generated images will be in results/dall-e-3-images, or in results/sd3.5-large-images, image metadata will be stored in results/megadata/image_megadata.csv."
     )
     # Loop through each combination of generation_type and farm_type
     for generation_type in generation_types:
