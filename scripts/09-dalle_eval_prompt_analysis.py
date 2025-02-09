40a399ab8a46ed197ce783090882862c63050bd9
Sky <82303914+skysheng7@users.noreply.github.com>
2025-02-09 10:02:16 -0800

    updated dalle eval prompt analysis

--- scripts/09-dalle_eval_prompt_analysis.py
+++ scripts/09-dalle_eval_prompt_analysis.py
@@ -16,30 +16,57 @@ prompts["prompts"] = prompts["prompts"].str.lower()
 
 # define cow or cattle related terms
 cattle_terms = ["cow", "cows", "cattle", "cattles"]
-pig_terms = [" pig ", " pigs "]
+cattle_neg_terms = ["cowboy", "statue of a cow"]
+pig_terms = ["pig", "pigs"]
+pig_neg_terms = ["pigeon", "guinea pig"]
 
 # Filter for pig and cow related prompts
 cattle_prompts = prompts[prompts["prompts"].str.contains("|".join(cattle_terms))]
+cattle_prompts = cattle_prompts[
+    ~cattle_prompts["prompts"].str.contains("|".join(cattle_neg_terms))
+]
 pig_prompts = prompts[prompts["prompts"].str.contains("|".join(pig_terms))]
-pig_prompts = pig_prompts[~pig_prompts["prompts"].str.contains("guinea")]
+pig_prompts = pig_prompts[~pig_prompts["prompts"].str.contains("|".join(pig_neg_terms))]
 n_cow = len(cattle_prompts)
 n_pig = len(pig_prompts)
 
 # cattle prompt related to pastre, grass
-pasture_terms = ["pasture", "grass", "green field", "lush green", "grazing"]
-cattle_pasture_prompts = cattle_prompts[
-    cattle_prompts["prompts"].str.contains("|".join(pasture_terms))
+outdoor_terms = [
+    "pasture",
+    "grass",
+    "green field",
+    "lush green",
+    "grazing",
+    "graze",
+    "field",
+    "beach",
+    "bushes",
+    "meadow",
 ]
-not_cattle_pasture_prompts = cattle_prompts[
-    ~cattle_prompts["prompts"].str.contains("|".join(pasture_terms))
+yard_terms = ["fence", "yard"]
+indoor_terms = ["barn", "bars", "metal", "line", "pen"]
+
+cattle_outdoor_prompts = cattle_prompts[
+    cattle_prompts["prompts"].str.contains("|".join(outdoor_terms))
+]
+cattle_indoor_prompts = cattle_prompts[
+    cattle_prompts["prompts"].str.contains("|".join(indoor_terms))
+]
+cattle_yard_prompts = cattle_prompts[
+    cattle_prompts["prompts"].str.contains("|".join(yard_terms))
+]
+cattle_other_prompts = cattle_prompts[
+    ~cattle_prompts["prompts"].str.contains(
+        "|".join(outdoor_terms + indoor_terms + yard_terms)
+    )
 ]
 
 # print the prompt about pig
 print(f"{n_pig} prompt about pig found:")
 print(pig_prompts["prompts"].iloc[0])
 
-#
+# number of prompts about cows
 print(f"{n_cow} prompt about cows found:")
 
-for index, row in not_cattle_pasture_prompts.iterrows():
+for index, row in cattle_indoor_prompts.iterrows():
     print(row["prompts"])
